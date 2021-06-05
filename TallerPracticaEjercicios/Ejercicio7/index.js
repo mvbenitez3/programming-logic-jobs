b2152f114c49b9bcf5267cdf612c8b9eed260f11
Vanessa <mvbenitez3@gmail.com>
2021-06-05 13:39:41 -0500

    Se modifico los ejercicios 7 y 8

--- TallerPracticaEjercicios/Ejercicio7/index.js
+++ TallerPracticaEjercicios/Ejercicio7/index.js
@@ -1,20 +1,21 @@
 /*
-7. Escriba un programa que solicite una contraseña (el texto de la contraseña no es importante) y la vuelva a solicitar hasta que las dos contraseñas coincidan.
+    7. Escriba un programa que solicite una contraseña (el texto de la contraseña no es importante) y la vuelva a solicitar hasta que las dos contraseñas coincidan.
 */
-verifyPassword();
+app();
 
-function verifyPassword(){
+function app (){
     let flag = false;
-    let passwor1;
-    let passwor2;
     while (!flag) {
-        passwor1 =  prompt("Escriba su contraseña");
-        passwor2 = prompt("Escriba de nuevo su contraseña");
-        if (passwor1 === passwor2) {
+        pass1 =  prompt("Escriba su contraseña");
+        pass2 = prompt("Escriba de nuevo su contraseña");
+        if ( verifyPassword(pass1, pass2)) {
             flag = true;
         }else{
             console.log("La contraseña no coincide");
         }
     }
-    console.log("¡HASTA LA VISTA!");
+    console.log("¡HASTA LA VISTA!");   
+}
+function verifyPassword(pass1, pass2){
+    return pass1 === pass2;
 }
--- TallerPracticaEjercicios/Ejercicio8/index.js
+++ TallerPracticaEjercicios/Ejercicio8/index.js
@@ -1,10 +1,9 @@
 /*
     8. Escriba un programa que simule una hucha. El programa solicitará primero una cantidad, que será la cantidad de dinero que queremos ahorrar. A continuación, el programa solicitará una y otra vez las cantidades que se irán ahorrando, hasta que el total ahorrado iguale o supere al objetivo. El programa no comprobará que las cantidades sean positivas.
 */
-
-simulateHucha();
-function simulateHucha(){
-    let amount = parseFloat(prompt("¿Cuántos euros quiere ahorrar?:"));
+let amount2 = parseFloat(prompt("¿Cuántos euros quiere ahorrar?:"));
+simulateHucha(amount2);
+function simulateHucha(amount){
     let hucha = 0; 
     while (hucha < amount) {
         hucha+= parseFloat(prompt("¿Cuántos euros quiere meter en la hucha?:"));
