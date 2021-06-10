c2db8c72a16b27c9fb303149da58f94f64debb09
Vanessa <mvbenitez3@gmail.com>
2021-06-10 09:13:22 -0500

    Se modifico el ejercicio 68 de la carpeta TallerPractica

--- TallerPractica/Ejercicio68/index.js
+++ TallerPractica/Ejercicio68/index.js
@@ -12,59 +12,52 @@ let enterAmount = parseInt(prompt("Ingrese cantidad de euros"));
 calulateBreakdowonBillsCoins(enterAmount);
 
 function calulateBreakdowonBillsCoins(amount) {
-  let bills500 = 0;
-  let bills200 = 0;
-  let bills100 = 0;
-  let bills50 = 0;
-  let bills20 = 0;
-  let bills10 = 0;
-  let bills5 = 0;
-  let coins2 = 0;
-  let coins1 = 0;
+  let bills = 0;
+  let coins = 0;
 
   if (amount >= 500) {
-    bills500 = parseInt(amount / 500);
-    amount = amount - bills500 * 500;
-    console.log(`${bills500} billetes de 500 euros.`);
+    bills = parseInt(amount / 500);
+    amount = amount - bills * 500;
+    console.log(`${bills} billetes de 500 euros.`);
   }
   if (amount >= 200) {
-    bills200 = parseInt(amount / 200);
-    amount = amount - bills200 * 200;
-    console.log(`${bills200} billetes de 200 euros.`);
+    bills = parseInt(amount / 200);
+    amount = amount - bills * 200;
+    console.log(`${bills} billetes de 200 euros.`);
   }
   if (amount >= 100) {
-    bills100 = parseInt(amount / 100);
-    amount = amount - bills100 * 100;
-    console.log(`${bills100} billetes de 100 euros.`);
+    bills = parseInt(amount / 100);
+    amount = amount - bills * 100;
+    console.log(`${bills} billetes de 100 euros.`);
   }
   if (amount >= 50) {
-    bills50 = parseInt(amount / 50);
-    amount = amount - bills50 * 50;
-    console.log(`${bills50} billetes de 50 euros.`);
+    bills = parseInt(amount / 50);
+    amount = amount - bills * 50;
+    console.log(`${bills} billetes de 50 euros.`);
   }
   if (amount >= 20) {
-    bills20 = parseInt(amount / 20);
-    amount = amount - bills20 * 20;
-    console.log(`${bills20} billetes de 20 euros.`);
+    bills = parseInt(amount / 20);
+    amount = amount - bills * 20;
+    console.log(`${bills} billetes de 20 euros.`);
   }
   if (amount >= 10) {
-    bills10 = parseInt(amount / 10);
-    amount = amount - bills10 * 10;
-    console.log(`${bills10} billetes de 10 euros.`);
+    bills = parseInt(amount / 10);
+    amount = amount - bills * 10;
+    console.log(`${bills} billetes de 10 euros.`);
   }
   if (amount >= 5) {
-    bills5 = parseInt(amount / 5);
-    amount = amount - bills5 * 5;
-    console.log(`${bills5} billetes de 5 euros.`);
+    bills = parseInt(amount / 5);
+    amount = amount - bills * 5;
+    console.log(`${bills} billetes de 5 euros.`);
   }
   if (amount >= 2) {
-    coins2 = parseInt(amount / 2);
-    amount = amount - coins2 * 2;
-    console.log(`${coins2} monedas de 2 euros.`);
+    coins = parseInt(amount / 2);
+    amount = amount - coins * 2;
+    console.log(`${coins} monedas de 2 euros.`);
   }
   if (amount >= 1) {
-    coins1 = parseInt(amount / 1);
-    amount = amount - coins1 * 1;
-    console.log(`${coins1} moneda de 1 euro.`);
+    coins = parseInt(amount / 1);
+    amount = amount - coins * 1;
+    console.log(`${coins} moneda de 1 euro.`);
   }
 }
